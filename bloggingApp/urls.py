b602316abf2503ae119b8c2158be9f6ece793e50
Briam <briammartinez@gmail.com>
2019-05-23 01:30:50 +0200

    refactor: llamando a las clases en el archivo url

--- bloggingApp/urls.py
+++ bloggingApp/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 from django.urls import path
+from posts import views
 from posts import views as post_views
 from users import views as users_views
 
@@ -11,10 +12,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', post_views.list_posts, name='feed'),
-    path('posts/new/', post_views.create_post, name='create_post'),
+    path('posts/new/', views.CreatePostView.as_view(), name='create_post'),
+    path('posts/<str:username>/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
 
     path('login/', users_views.login_view, name='login'),
     path('logout', users_views.logout_view, name='logout'),
-    path('signup', users_views.signup, name='signup'),
+    path('signup', users_views.SignupView.as_view(), name='signup'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

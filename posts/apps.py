c2cfe3952579f002e95e43e45de538bb6318b39d
Briam <briammartinez@gmail.com>
2019-05-24 19:49:31 +0200

    Añadidos archivos base de posts y de users

---
+++ posts/apps.py
@@ -0,0 +1,9 @@
+"""Post application module"""
+
+from django.apps import AppConfig
+
+
+class PostsConfig(AppConfig):
+    """Post application setting"""
+    name = 'posts'
+    varbose_name = 'Posts'

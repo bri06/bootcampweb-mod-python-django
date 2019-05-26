07a10072bb57ed22a8ff0626ae54d42e603fd0d2
Briam <briammartinez@gmail.com>
2019-05-26 16:14:45 +0200

    Añadido filtro de busqueda y ordering al api de posts

--- posts/api.py
+++ posts/api.py
@@ -1,17 +1,36 @@
+"""Posts api"""
+
+# rest_framework
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
+# Posts
 from posts.models import Post
 from posts.serializers import PostListSerializer, PostSerializer
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from posts.permissions import PostPermission
 
 
 class PostsAPI(ListCreateAPIView):
 
-  queryset = Post.objects.all()
-  serializer_class = PostSerializer
+  permission_classes = [PostPermission]
 
+  filter_backends = [SearchFilter, OrderingFilter]
+  search_fields = ['title', 'photo', 'user__first_name', 'user__last_name']
+  ordering_fields = ['title', 'created']
+
+  queryset = Post.objects.all()
+
+  serializer_class = PostSerializer
 
   def get_serializer_class(self):
     return PostListSerializer if self.request.method == 'GET' else PostSerializer
 
+
 class PostsDetailAPI(RetrieveUpdateDestroyAPIView):
-  queryset = Post.objects.all()
+
+  permission_classes = [PostPermission]
+
   serializer_class = PostSerializer
+
+  def perform_update(self, serializer):
+    serializer.save(user=self.request.user)

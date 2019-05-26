89bbca6a87a1bc8c6c6eee5b061aa89225ccc09d
Briam <briammartinez@gmail.com>
2019-05-26 02:30:55 +0200

    Primera version del api de users

---
+++ users/api.py
@@ -0,0 +1,56 @@
+"""Api of users"""
+
+# Django
+from django.contrib.auth.models import User
+
+# rest_framework
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404, GenericAPIView
+from rest_framework import status
+
+
+# users
+from users.serializers import UserListSerializer, UserSerializer, WriteUserSerializer
+from users.permissions import UserPermission
+
+class UsersAPI(GenericAPIView):
+
+
+  def get(self, request):
+    users = User.objects.all()
+    serializer = UserListSerializer(users, many=True)
+    return Response(serializer.data)
+
+  def post(self, request):
+    serializer = WriteUserSerializer(data=request.data)
+    if serializer.is_valid():
+      new_user = serializer.save()
+      user_serializer = UserSerializer(new_user)
+      return Response(user_serializer.data, status=status.HTTP_201_CREATED)
+    else:
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserDetailAPI(APIView):
+
+  def get(self, request, pk):
+    """Get user detail"""
+    user = get_object_or_404(User, pk=pk)
+    serializer = UserSerializer(user)
+    return Response(serializer.data)
+
+  def delete(self, request, pk):
+    user = get_object_or_404(User, pk=pk)
+    user.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+  def put(self, request, pk):
+    user = get_object_or_404(User, pk=pk)
+    serializer = WriteUserSerializer(user, data=request.data)
+    if serializer.is_valid():
+      updated_user = serializer.save()
+      user_serializer = UserSerializer(updated_user)
+      return Response(user_serializer.data, status=status.HTTP_202_ACCEPTED)
+    else:
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
